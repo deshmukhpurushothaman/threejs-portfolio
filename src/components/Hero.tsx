ce64c5adeea07e1d3b0267fdf5e8b9446067feac
Deshmukh P <deshmukhpurushothaman@gmail.com>
2023-03-04 17:06:49 +0530

    🦸Hero section completed along with webpack config for lazy loading

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,6 +10,30 @@ const Hero = () => {
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Deshmukh P</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            Full Stack Developer, <br className="sm:block hidden" /> who is hunting and grasping new techs
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+      <div className="absolute xs:bottom-0 botton-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
       </div>
     </section>
   )
