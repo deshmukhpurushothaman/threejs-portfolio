d4bfec4437a6409903f3258880ec035b9b91d9a8
Deshmukh P <deshmukhpurushothaman@gmail.com>
2023-03-05 09:57:49 +0530

    ⚒️tech section and assets moved to public folder

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { styles } from '../styles/styles'
 import { navLinks } from '../constants'
-import { menu, close } from '../assets'
-import Logo from '../assets/mylogo.png'
+import { menu, close } from '../../public/assets'
+import Logo from '../../public/assets/mylogo.png'
 
 const Navbar = () => {
   const [active, setActive] = useState("")
