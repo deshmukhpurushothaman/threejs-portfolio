5be7dbc858bd11137a90e182c9567e4f8d19faf8
Deshmukh P <deshmukhpurushothaman@gmail.com>
2023-03-05 13:46:50 +0530

    🔁computer auto rotate feat

--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -64,7 +64,9 @@ const ComputersCanvas = () => {
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
-
+          enableRotate={true}
+          autoRotate={true}
+          autoRotateSpeed={1.5}
         />
         <Computers isMobile={isMobile} />
       </Suspense>
