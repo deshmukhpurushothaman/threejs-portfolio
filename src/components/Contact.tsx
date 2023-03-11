8495f6d12aeb060f0ccee8f27b615856abc84d20
Deshmukh P <deshmukhpurushothaman@gmail.com>
2023-03-11 12:23:54 +0530

    toast added

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -46,19 +46,21 @@ const Contact = () => {
         .then(
           () => {
             setLoading(false);
-            alert("Thank you. I will get back to you as soon as possible.");
-            // toast.info("Thank you. I will get back to you as soon as possible.")
+            // alert("Thank you. I will get back to you as soon as possible.");
+            toast.info("Thank you. I will get back to you as soon as possible.")
             setForm({
               name: "",
               email: "",
               message: "",
             });
+            return;
           },
           (error) => {
             setLoading(false);
             // console.error(error);
-            // toast.error("Ahh, something went wrong. Please try again.")
-            alert("Ahh, something went wrong. Please try again.");
+            toast.error("Ahh, something went wrong. Please try again.")
+            return
+            // alert("Ahh, something went wrong. Please try again.");
           }
         );
     } catch (error) {
